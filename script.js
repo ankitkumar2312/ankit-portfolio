e7d4fd2e84796c93eaf0cb563161c3e9fe885bbd
ankitkumar2312 <ankitkumar8130260@gmail.com>
2026-07-23 21:28:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,12 @@ const text = "Clinical Data Management Associate";
 
 let i = 0;
 
-function typeWriter(){
-
-    if(i < text.length){
-
-        document.getElementById("typing").innerHTML += text.charAt(i);
-
+function typeWriter() {
+    if (i < text.length) {
+        document.getElementById("typing").textContent += text.charAt(i);
         i++;
-
-        setTimeout(typeWriter,70);
-
+        setTimeout(typeWriter, 50);
     }
-
 }
 
-typeWriter();
+window.onload = typeWriter;
